451688cf054b7dae282763f84ebedc8d193f68c0
MoamenZakariaMohamed <92689083+MoamenZakariaMohamed@users.noreply.github.com>
2024-07-05 15:40:52 +0300

    add rate limiter

--- src/main/java/com/fault/tolerance/FaultController.java
+++ src/main/java/com/fault/tolerance/FaultController.java
@@ -1,9 +1,14 @@
 package com.fault.tolerance;
 
 
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,4 +24,11 @@ public class FaultController {
         log.info("Inside Controller");
         service.send("This is the body.");
     }
+
+
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(@RequestParam(value="name", defaultValue = "Hello") String name){
+        return service.rateTest(name);
+    }
+
 }
--- src/main/java/com/fault/tolerance/FaultService.java
+++ src/main/java/com/fault/tolerance/FaultService.java
@@ -1,11 +1,17 @@
 package com.fault.tolerance;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClient;
 
 @Service
@@ -26,6 +32,20 @@ public class FaultService {
 
 
     }
+
+
+    @RateLimiter(name = "getMessageRateLimit", fallbackMethod = "getMessageFallBack")
+    public  ResponseEntity<String> rateTest(String name) {
+        return ResponseEntity.ok().body("Message from getMessage() :" +name);
+    }
+    private ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+
+        log.info("Rate limit has applied, So no further calls are getting accepted");
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .body("Too many requests : No further request will be accepted. Please try after sometime");
+    }
+
 
     public void fallback(String msg, Exception ex){
         log.info("Inside Fallback for retry");
